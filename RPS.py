85b9781571f0304ec469f81c8fb3e75943c87dff
Crotaa <webbnathana@gmail.com>
2023-02-13 17:37:41 -0700

    hji2

--- RPS.py
+++ RPS.py
@@ -6,9 +6,7 @@ pygame.init()
 #variables and constants
 BG_COLOR = "#8760C9"
 player_1 = ""
-play_again = True
 running = True
-result_input = False
 screen = pygame.display.set_mode([800,600])
 type_dict = {"Rock": 1,"Paper": 2,"Scissors": 3}
 Comp_Rock = {1: "Tie", 2: "Win", 3: "Lose"}
@@ -28,7 +26,9 @@ rocky = screen.blit(rock_image, (50,200))
 scissorz = screen.blit(scissors_image, (550,200))
 papers = screen.blit(paper_image, (300,200))
 
+
 #functions to make interactable objects
+
 
 def text_func(text,x,y,font,size,color):
    set_my_font = pygame.font.SysFont(font, size)
@@ -37,19 +37,15 @@ def text_func(text,x,y,font,size,color):
 
 
 def random_to_type():
- computer_rand_int = random.randint(0,3)
- return computer_rand_int
+ return random.randint(1,3)
 
 
 def descision(rand_int, type):
  if rand_int == 1:
-   #chose = (f"\nComputer Chose Rock \n{Comp_Rock[type]}\n")
    winlosetie = Comp_Rock[type]
  elif rand_int == 2:
-   #chose = (f"\nComputer Chose Paper \n{Comp_Paper[type]}\n")
    winlosetie = Comp_Paper[type]
  else:
-   #chose = (f"\nComputer Chose Scissors \n{Comp_Scissor[type]}\n")
    winlosetie = Comp_Scissor[type]
  return winlosetie
 
@@ -81,32 +77,35 @@ while running:
  #process input
  if player_1 != "":
      result = descision(random_to_type(), type_dict[player_1])
- if result_input == True:
-   text_func("Play Again? Y or N",135,500,"arial",60,"black")
-   if event.type == pygame.KEYDOWN:
-     if event.key == pygame.K_n:
-       running = False
+     text_func(f"You Selected {player_1}!",135,200,"arial",60,"black")
+     text_func(f"You {result}!",135,500,"arial",60,"black")
+     text_func("Play Again? Y or N",135,450,"arial",60,"black")
+     run = False
+     while run == False:
+       print("hello")
+       for event in pygame.event.get(): 
+         if event.type == pygame.KEYDOWN:
+           if event.key == pygame.K_n:
+             running = False
+             run = True
+           elif event.key == pygame.K_y:
+             player_1 = ""
+             run = True
 
 
  #update game state
  screen.fill(BG_COLOR)
- if player_1 == "":
-   rocky
-   scissorz
-   papers
- if player_1 != "":
-     text_func(f"You Selected {player_1}!",135,500,"arial",60,"black")
-     while result_input == False:
-       text_func("Are you ready to see your results? Type Y!",135,500,"arial",60,"black")
-       if event.type == pygame.KEYDOWN:
-         if event.key == pygame.K_y:
-           text_func(f"You {result}!",135,500,"arial",60,"black")
-           result_input = True
+ rocky = screen.blit(rock_image, (50,200))
+ scissorz = screen.blit(scissors_image, (550,200))
+ papers = screen.blit(paper_image, (300,200))
+ text_func("Select an Option",135,450,"arial",60,"black")
         
  #draw screen
  pygame.display.flip()
 
 
 pygame.quit()
+
+
 
 
